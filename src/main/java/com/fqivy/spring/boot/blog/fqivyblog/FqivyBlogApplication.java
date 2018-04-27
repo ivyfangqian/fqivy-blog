ae69f2a8474299528c023531bbfab231116bf7f3
ivyfangqian <ivy_fqivy@126.com>
2018-04-27 16:59:09 +0800

    the first commit

---
+++ src/main/java/com/fqivy/spring/boot/blog/fqivyblog/FqivyBlogApplication.java
@@ -0,0 +1,12 @@
+package com.fqivy.spring.boot.blog.fqivyblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FqivyBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FqivyBlogApplication.class, args);
+    }
+}
